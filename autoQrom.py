74258e53ca28c73cfe67ae4510aac9b1eb3a7e2a
Riiiichi <richard.gunawan745@gmail.com>
2026-01-17 02:06:39 +1100

    Remove all of the debugging code

--- autoQrom.py
+++ autoQrom.py
@@ -16,7 +16,6 @@ def generate_qrom_circuit(bitstrings):
         
     num_ctrl_qubits =  math.ceil(math.log(num_of_elements, 2))
 
-    print(f"Number of control qubits: {num_ctrl_qubits}")
 
     ctrl_reg = cirq.LineQubit.range(num_ctrl_qubits)  # Control qubits
     trgt_reg = cirq.LineQubit.range(num_ctrl_qubits, num_ctrl_qubits + num_target_qubits)  # Target qubits
@@ -28,17 +27,14 @@ def generate_qrom_circuit(bitstrings):
     # Use multi-controlled X gates to set the target qubits based on the control qubits
     for index, bitstring in enumerate(bitstrings):
         part_circuit = cirq.Circuit()
-        print(f"Processing bitstring index {index}: {bitstring}")
         i_in_binary = bin(index)[2:].zfill(num_ctrl_qubits) 
-        print(f"Control bits: {i_in_binary}")
 
         #Creating anti-controls
         for i, bit in enumerate(i_in_binary):
             if bit == "0":
                 part_circuit.append(cirq.X(ctrl_reg[i]))
-        
-        print(f"Applying X gates to control qubits: {[ctrl_reg[i] for i, bit in enumerate(i_in_binary) if bit == '0']}")
 
+        #Setting target bits
         for target_i, target_bit in enumerate(bitstring):
             if target_bit == "1":
                 part_circuit.append(cirq.X(trgt_reg[target_i]).controlled_by(*ctrl_reg))
@@ -47,7 +43,7 @@ def generate_qrom_circuit(bitstrings):
         for i, bit in enumerate(i_in_binary):
             if bit == "0":
                  part_circuit.append(cirq.X(ctrl_reg[i]))
-                 
+
         circuit += part_circuit
 
 
